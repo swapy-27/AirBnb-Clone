32e638a42cf64351086128fd819378b912158a01
Swapnil Jain <swapniljain240@gmail.com>
2023-01-05 16:25:28 +0530

    added serach bar in header

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 import logo from "../public/images/airbnbLogo.png"
+import  {MagnifyingGlassIcon}  from '@heroicons/react/24/solid'
 export default function Header() {
   return (
     <header className='grid sticky top-0 z-50 grid-cols-3 bg-white shadow-md py-3 px-5  my-auto'>
@@ -10,8 +11,12 @@ export default function Header() {
         </div>
 
         {/* Center */}
-        <div>
-            <input name='Search here' placeholder='Search something here'/>
+        <div className='flex items-center border-2 
+                        rounded-full py-2 shadow-md'>
+            <input className='flex-grow bg-transparent pl-5 outline-none' 
+                    typeof='text' placeholder='Search something here'/>
+            <MagnifyingGlassIcon className='hidden md:inline-flex h-8
+             bg-red-400 text-white rounded-full p-2 cursor-pointer  md:mx-2' />
         </div>
 
         {/* Right */}
